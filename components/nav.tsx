60e652661d804282891afac7ea888bffae026593
Zakusei <zakusei.dev@gmail.com>
2024-02-23 23:33:07 +0800

    initial commit

--- components/nav.tsx
+++ components/nav.tsx
@@ -26,8 +26,9 @@ const NavigationBar = () => {
     <section id="home" className="container p-12 md:px-24 md:pt-24">
       <nav>
         <ul className="grid grid-cols-4 place-items-start max-w-xl w-full font-bold sm:text-xl lg:text-2xl">
-          {navlinks.map(({ title, href }) => (
+          {navlinks.map(({ title, href }, index) => (
             <li
+              key={index}
               className={`flex flex-col border-main ${
                 path != href
                   ? "hover:text-main-gradient-t text-muted-foreground/20"
