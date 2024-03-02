f730fa9c8b70bd0c12feb99b080781b9013306ce
Zakusei <zakusei.dev@gmail.com>
2024-03-03 02:39:05 +0800

    add firebase config.ts

---
+++ db/firebase-config.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtSdRoQ_J8QciU94QFDG0L8dHF8I4TMnU",
+  authDomain: "sabrheas.firebaseapp.com",
+  projectId: "sabrheas",
+  storageBucket: "sabrheas.appspot.com",
+  messagingSenderId: "926440283766",
+  appId: "1:926440283766:web:70db95e8ea89d3f90dc957"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
