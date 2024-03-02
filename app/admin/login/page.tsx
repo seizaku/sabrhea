2df715fdd300463af40277ed07e24d567b1a58c7
Zakusei <zakusei.dev@gmail.com>
2024-03-03 02:38:25 +0800

    login ui

---
+++ app/admin/login/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { MainContainer } from "@/components/main-container";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { ButtonPrimary } from "@/components/button-primary";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <MainContainer className="max-w-sm mt-44">
+      <section className="relative rounded-b-3xl bg-white drop-shadow-sm h-fit p-12 flex justify-between items-center">
+        <form action="" className="w-full mx-auto space-y-2">
+          <div className="grid gap-1.5">
+            <Label>Username</Label>
+            <Input />
+          </div>
+          <div className="grid gap-1.5">
+            <Label>Password</Label>
+            <Input />
+          </div>
+          <ButtonPrimary>Login</ButtonPrimary>
+        </form>
+      </section>
+    </MainContainer>
+  );
+}
